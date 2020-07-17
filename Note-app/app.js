6062844f99d48181bae1b1b708735c491d70a0cc
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-17 16:01:56 +0530

    udpate

--- Note-app/app.js
+++ Note-app/app.js
@@ -1,3 +1,45 @@
-const val = require('validator');
+// Module Imports
+const val = require("validator");
+const chalk = require("chalk");
+const yargs = require("yargs");
 
-console.log(c);
+// Setting version
+yargs.version('1.0.0')
+
+// Add Command
+yargs.command({
+    command:"add",
+    describe:"Adds a new Note",
+    handler: function () {
+        console.log('New Note Added');
+    }
+})
+
+// Remove Command
+yargs.command({
+    command:"remove",
+    describe:"Removes a Note",
+    handler: function () {
+        console.log('Note Removed');
+    }
+})
+
+// List Command
+yargs.command({
+    command:"list",
+    describe:"lists the note",
+    handler: function () {
+        console.log('Lists Of Notes');
+    }
+})
+
+// Read Command
+yargs.command({
+    command:"read",
+    describe:"Reads a note",
+    handler: function () {
+        console.log('Read notes');
+    }
+})
+
+console.log(yargs.argv);
