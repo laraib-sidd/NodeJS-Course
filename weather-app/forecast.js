a0d52215fc2599a0b1ecc2d0915d3c8654bd36b0
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-18 22:22:33 +0530

    update

--- weather-app/forecast.js
+++ weather-app/forecast.js
@@ -4,18 +4,20 @@ const fetchData = (query, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=ed2dfdec63a7e1f209ca33f11a7ec204&query=${encodeURIComponent(query)}`;
     setTimeout(() => {
         request({
-            url: url,
+            url,
             json: true
-        }, (err, res) => {
+        }, (err, {
+            body
+        }) => {
             if (err) {
                 callback("Check your internet Connection")
-            } else if (res.body.error) {
+            } else if (body.error) {
                 callback("Check your input Query");
             } else {
                 callback(undefined, {
-                    temperature: res['body']['current']['temperature'],
-                    weather: res.body.current.weather_descriptions[0],
-                    location: res['body']['request']['query']
+                    temperature: body.current.temperature,
+                    weather: body.current.weather_descriptions[0],
+                    location: body.request.query
                 })
             }
         })
