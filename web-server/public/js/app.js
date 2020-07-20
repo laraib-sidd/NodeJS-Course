ae1eabffca1f665fd43e6208b05e6dda1b962303
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-20 18:46:19 +0530

    update

---
+++ web-server/public/js/app.js
@@ -0,0 +1,41 @@
+// let intt;
+// var inp = document.querySelector('input');
+// inp.addEventListener('input', (e) => {
+//     intt = e.target.value
+// })
+
+// var btn = document.querySelector('button');
+// btn.addEventListener('click', (e) => {
+    // url = `http://localhost:3003/weather?address=d`
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then((data) => {
+    //         if (data.error) {
+    //             console.log(data.error);
+    //         } else {
+    //             console.log(data);
+    //         }
+    //     })
+// })
+
+
+var weatherForm = document.querySelector('form')
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    var int = e.target[0].value
+    url = `http://localhost:3003/weather?address=${int}`
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data);
+            }
+        })
+})
+
+// url = 'http://puzzle.mead.io/puzzle'
+// fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data)) 
